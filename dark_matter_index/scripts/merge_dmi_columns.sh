fad4ef6631863a1e629ee8be6f9f2ed36fc7d1ff
David Koslicki <dmkoslicki@gmail.com>
2026-01-21 15:13:16 -0500

    add analysis scripts #8: NEED TO REMOVE HOST HASHES

--- dark_matter_index/scripts/merge_dmi_columns.sh
+++ dark_matter_index/scripts/merge_dmi_columns.sh
@@ -6,3 +6,8 @@ python merge_dmi_columns.py \
     --parquet-without-dmi /scratch/dmk333_new/Logan/Logan_Analyses/verify_diversity_correlation/data/hash_diversity_results_full_cov_0.0/analysis/filtered_analysis_min_hash_10000_min_diversity_10/filtered_data.parquet \
     --output /scratch/dmk333_new/Logan/Logan_Analyses/verify_diversity_correlation/data/hash_diversity_results_full_cov_0.0/analysis/filtered_analysis_min_hash_10000_min_diversity_10/filtered_data_with_dmi_merged.parquet
 fi
+
+python merge_dmi_columns.py \
+    --parquet-with-dmi /scratch/dmk333_new/Logan/Logan_Analyses/verify_diversity_correlation/data/hash_diversity_results_full_cov_0.0/data/hash_diversity_data_with_dmi.parquet \
+    --parquet-without-dmi /scratch/dmk333_new/Logan/Logan_Analyses/verify_diversity_correlation/data/hash_diversity_results_full_cov_0.0/analysis/filtered_analysis_min_mbases_1620_min_diversity_10/filtered_data.parquet \
+    --output /scratch/dmk333_new/Logan/Logan_Analyses/verify_diversity_correlation/data/hash_diversity_results_full_cov_0.0/analysis/filtered_analysis_min_mbases_1620_min_diversity_10/filtered_data_with_dmi_merged.parquet
